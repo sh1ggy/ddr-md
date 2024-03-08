2f8048f2247eae7c556e16c47b0bb555fad99ca2
sh1ggy <tyronewessnolasco@gmail.com>
2024-03-08 22:50:23 +1000

    JSON changes, detail widgetisation & more info

--- lib/components/song.dart
+++ lib/components/song.dart
@@ -39,68 +39,93 @@ class _SongPageState extends State<SongPage> {
         return Directionality(
           textDirection: TextDirection.ltr,
           child: Scaffold(
-            body: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Image(
-                    image: AssetImage('assets/background.png'),
-                    height: 100,
-                  ),
-                  const SizedBox(width: 20),
-                  if (songInfo != null)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          songInfo!.name,
-                          softWrap: true,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        Text(
-                          songInfo!.version,
-                          style: const TextStyle(fontStyle: FontStyle.italic),
-                        ),
-                        Text((formattedTime(
-                                timeInSecond: songInfo!.songLength.toInt()) +
-                            " min")),
-                        Row(
-                          children: [
-                            Text(
-                              songInfo!.levels.single.beginner.toString(),
-                              style: const TextStyle(
-                                  color: Colors.cyan,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              songInfo!.levels.single.easy.toString(),
-                              style: const TextStyle(
-                                  color: Colors.orange,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              songInfo!.levels.single.medium.toString(),
-                              style: const TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(songInfo!.levels.single.hard.toString(),
-                                style: const TextStyle(
-                                    color: Colors.purple,
-                                    fontWeight: FontWeight.bold)),
-                          ]
-                              .expand((x) => [const SizedBox(width: 10), x])
-                              .skip(1)
-                              .toList(),
-                        )
-                      ],
-                    ),
-                ]),
+            body: songDetails(),
           ),
         );
       }),
     );
+  }
+
+  Row songDetails() {
+    return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Image(
+                  image: AssetImage('assets/background.png'),
+                  height: 100,
+                ),
+                const SizedBox(width: 20),
+                if (songInfo != null)
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        songInfo!.name,
+                        softWrap: true,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text(
+                        songInfo!.version,
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.black,
+                          ),
+                          children: <TextSpan>[
+                            const TextSpan(
+                                text: 'Length: ',
+                                style:
+                                    TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(
+                                text: (formattedTime(
+                                        timeInSecond:
+                                            songInfo!.songLength.toInt()) +
+                                    " min, ")),
+                            const TextSpan(
+                                text: 'BPM: ',
+                                style:
+                                    TextStyle(fontWeight: FontWeight.bold)),
+                            TextSpan(text: songInfo!.chart.bpmRange),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            songInfo!.levels.single.beginner.toString(),
+                            style: const TextStyle(
+                                color: Colors.cyan,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            songInfo!.levels.single.easy.toString(),
+                            style: const TextStyle(
+                                color: Colors.orange,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            songInfo!.levels.single.medium.toString(),
+                            style: const TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(songInfo!.levels.single.hard.toString(),
+                              style: const TextStyle(
+                                  color: Colors.purple,
+                                  fontWeight: FontWeight.bold)),
+                        ]
+                            .expand((x) => [const SizedBox(width: 10), x])
+                            .skip(1)
+                            .toList(),
+                      ),
+                    ],
+                  ),
+              ]);
   }
 }
--- lib/components/songJson.dart
+++ lib/components/songJson.dart
@@ -9,29 +9,29 @@ SongInfo parseJson(String str) => SongInfo.fromJson(json.decode(str));
 String welcomeToJson(SongInfo data) => json.encode(data.toJson());
 
 class SongInfo {
-    bool ssc;
-    String version;
-    String name;
-    String title;
-    String titletranslit;
-    double songLength;
-    bool perChart;
-    Levels levels;
-    List<Chart> chart;
+  bool ssc;
+  String version;
+  String name;
+  String title;
+  String titletranslit;
+  double songLength;
+  bool perChart;
+  Levels levels;
+  Chart chart;
 
-    SongInfo({
-        required this.ssc,
-        required this.version,
-        required this.name,
-        required this.title,
-        required this.titletranslit,
-        required this.songLength,
-        required this.perChart,
-        required this.levels,
-        required this.chart,
-    });
+  SongInfo({
+    required this.ssc,
+    required this.version,
+    required this.name,
+    required this.title,
+    required this.titletranslit,
+    required this.songLength,
+    required this.perChart,
+    required this.levels,
+    required this.chart,
+  });
 
-    factory SongInfo.fromJson(Map<String, dynamic> json) => SongInfo(
+  factory SongInfo.fromJson(Map<String, dynamic> json) => SongInfo(
         ssc: json["ssc"],
         version: json["version"],
         name: json["name"],
@@ -40,10 +40,10 @@ class SongInfo {
         songLength: json["song_length"]?.toDouble(),
         perChart: json["per_chart"],
         levels: Levels.fromJson(json["levels"]),
-        chart: List<Chart>.from(json["chart"].map((x) => Chart.fromJson(x))),
-    );
+        chart: Chart.fromJson(json["chart"]),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "ssc": ssc,
         "version": version,
         "name": name,
@@ -52,114 +52,114 @@ class SongInfo {
         "song_length": songLength,
         "per_chart": perChart,
         "levels": levels.toJson(),
-        "chart": List<dynamic>.from(chart.map((x) => x.toJson())),
-    };
+        "chart": chart.toJson(),
+      };
 }
 
 class Chart {
-    int dominantBpm;
-    int trueMin;
-    int trueMax;
-    String bpmRange;
-    List<Bpm> bpms;
-    List<dynamic> stops;
+  int dominantBpm;
+  int trueMin;
+  int trueMax;
+  String bpmRange;
+  List<Bpm> bpms;
+  List<dynamic> stops;
 
-    Chart({
-        required this.dominantBpm,
-        required this.trueMin,
-        required this.trueMax,
-        required this.bpmRange,
-        required this.bpms,
-        required this.stops,
-    });
+  Chart({
+    required this.dominantBpm,
+    required this.trueMin,
+    required this.trueMax,
+    required this.bpmRange,
+    required this.bpms,
+    required this.stops,
+  });
 
-    factory Chart.fromJson(Map<String, dynamic> json) => Chart(
+  factory Chart.fromJson(Map<String, dynamic> json) => Chart(
         dominantBpm: json["dominant_bpm"],
         trueMin: json["true_min"],
         trueMax: json["true_max"],
         bpmRange: json["bpm_range"],
         bpms: List<Bpm>.from(json["bpms"].map((x) => Bpm.fromJson(x))),
         stops: List<dynamic>.from(json["stops"].map((x) => x)),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "dominant_bpm": dominantBpm,
         "true_min": trueMin,
         "true_max": trueMax,
         "bpm_range": bpmRange,
         "bpms": List<dynamic>.from(bpms.map((x) => x.toJson())),
         "stops": List<dynamic>.from(stops.map((x) => x)),
-    };
+      };
 }
 
 class Bpm {
-    double st;
-    double ed;
-    int val;
+  double st;
+  double ed;
+  int val;
 
-    Bpm({
-        required this.st,
-        required this.ed,
-        required this.val,
-    });
+  Bpm({
+    required this.st,
+    required this.ed,
+    required this.val,
+  });
 
-    factory Bpm.fromJson(Map<String, dynamic> json) => Bpm(
+  factory Bpm.fromJson(Map<String, dynamic> json) => Bpm(
         st: json["st"],
         ed: json["ed"]?.toDouble(),
         val: json["val"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "st": st,
         "ed": ed,
         "val": val,
-    };
+      };
 }
 
 class Levels {
-    Double single;
-    Double levelsDouble;
+  Double single;
+  Double levelsDouble;
 
-    Levels({
-        required this.single,
-        required this.levelsDouble,
-    });
+  Levels({
+    required this.single,
+    required this.levelsDouble,
+  });
 
-    factory Levels.fromJson(Map<String, dynamic> json) => Levels(
+  factory Levels.fromJson(Map<String, dynamic> json) => Levels(
         single: Double.fromJson(json["single"]),
         levelsDouble: Double.fromJson(json["double"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "single": single.toJson(),
         "double": levelsDouble.toJson(),
-    };
+      };
 }
 
 class Double {
-    int easy;
-    int medium;
-    int hard;
-    int? beginner;
+  int easy;
+  int medium;
+  int hard;
+  int? beginner;
 
-    Double({
-        required this.easy,
-        required this.medium,
-        required this.hard,
-        this.beginner,
-    });
+  Double({
+    required this.easy,
+    required this.medium,
+    required this.hard,
+    this.beginner,
+  });
 
-    factory Double.fromJson(Map<String, dynamic> json) => Double(
+  factory Double.fromJson(Map<String, dynamic> json) => Double(
         easy: json["easy"],
         medium: json["medium"],
         hard: json["hard"],
         beginner: json["beginner"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "easy": easy,
         "medium": medium,
         "hard": hard,
         "beginner": beginner,
-    };
+      };
 }
