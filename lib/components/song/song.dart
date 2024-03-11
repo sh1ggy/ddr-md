71cd4367a18233a965c5cfbee625f1952ab006ff
sh1ggy <tyronewessnolasco@gmail.com>
2024-03-11 16:42:02 +1000

    Conditional BPM changes

--- lib/components/song/song.dart
+++ lib/components/song/song.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:ddr_md/components/song/note.dart';
 import 'package:ddr_md/components/songJson.dart';
+import 'package:ddr_md/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 formattedTime({required int timeInSecond}) {
   int sec = timeInSecond % 60;
@@ -22,11 +24,15 @@ class SongPage extends StatefulWidget {
 
 class _SongPageState extends State<SongPage> {
   SongInfo? songInfo;
+  double mod = 1.0;
+  bool? isBpmChange;
+
   Future<void> readSongJson() async {
     final String response = await rootBundle.loadString('assets/50th.json');
     final data = await json.decode(response);
     setState(() {
       songInfo = parseJson(response);
+      isBpmChange = songInfo!.chart.trueMax != songInfo!.chart.trueMin;
     });
   }
 
@@ -42,8 +48,9 @@ class _SongPageState extends State<SongPage> {
           child: Scaffold(
             body: Column(
               children: [
-                songDetails(),
+                if (songInfo != null) songDetails(),
                 note(),
+                if (songInfo != null && isBpmChange != null) songBpm(),
               ].expand((x) => [const SizedBox(height: 20), x]).skip(1).toList(),
             ),
           ),
@@ -63,68 +70,136 @@ class _SongPageState extends State<SongPage> {
             height: 100,
           ),
           const SizedBox(width: 20),
-          if (songInfo != null)
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  songInfo!.name,
-                  softWrap: true,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                songInfo!.name,
+                softWrap: true,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+              ),
+              Text(
+                songInfo!.version,
+                style: const TextStyle(fontStyle: FontStyle.italic),
+              ),
+              RichText(
+                text: TextSpan(
                   style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 15),
-                ),
-                Text(
-                  songInfo!.version,
-                  style: const TextStyle(fontStyle: FontStyle.italic),
-                ),
-                RichText(
-                  text: TextSpan(
-                    style: const TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.black,
-                    ),
-                    children: <TextSpan>[
-                      const TextSpan(
-                          text: 'Length: ',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text: (formattedTime(
-                                  timeInSecond: songInfo!.songLength.toInt()) +
-                              " min, ")),
-                      const TextSpan(
-                          text: 'BPM: ',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(text: songInfo!.chart.bpmRange),
-                    ],
+                    fontSize: 14.0,
+                    color: Colors.black,
                   ),
+                  children: <TextSpan>[
+                    const TextSpan(
+                        text: 'Length: ',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(
+                        text: (formattedTime(
+                                timeInSecond: songInfo!.songLength.toInt()) +
+                            " min, ")),
+                    const TextSpan(
+                        text: 'BPM: ',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    TextSpan(text: songInfo!.chart.bpmRange),
+                  ],
                 ),
-                Row(
-                  children: [
-                    Text(
-                      songInfo!.levels.single.beginner.toString(),
+              ),
+              Row(
+                children: [
+                  Text(
+                    songInfo!.levels.single.beginner.toString(),
+                    style: const TextStyle(
+                        color: Colors.cyan, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    songInfo!.levels.single.easy.toString(),
+                    style: const TextStyle(
+                        color: Colors.orange, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    songInfo!.levels.single.medium.toString(),
+                    style: const TextStyle(
+                        color: Colors.green, fontWeight: FontWeight.bold),
+                  ),
+                  Text(songInfo!.levels.single.hard.toString(),
                       style: const TextStyle(
-                          color: Colors.cyan, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      songInfo!.levels.single.easy.toString(),
-                      style: const TextStyle(
-                          color: Colors.orange, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      songInfo!.levels.single.medium.toString(),
-                      style: const TextStyle(
-                          color: Colors.green, fontWeight: FontWeight.bold),
-                    ),
-                    Text(songInfo!.levels.single.hard.toString(),
-                        style: const TextStyle(
-                            color: Colors.purple, fontWeight: FontWeight.bold)),
-                  ]
-                      .expand((x) => [const SizedBox(width: 10), x])
-                      .skip(1)
-                      .toList(),
-                ),
-              ],
-            ),
+                          color: Colors.purple, fontWeight: FontWeight.bold)),
+                ]
+                    .expand((x) => [const SizedBox(width: 10), x])
+                    .skip(1)
+                    .toList(),
+              ),
+            ],
+          ),
         ]);
+  }
+
+  Container songBpm() {
+    var appState = context.watch<MyAppState>();
+    return Container(
+      padding: const EdgeInsets.all(7.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                  width: 50,
+                  child: Text(
+                    'Avg',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+              const SizedBox(width: 30),
+              if (isBpmChange!) ...[
+                const SizedBox(
+                    width: 50,
+                    child: Text(
+                      'Min',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )),
+                const SizedBox(width: 30),
+                const SizedBox(
+                    width: 50,
+                    child: Text(
+                      'Max',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )),
+                const SizedBox(width: 30),
+              ],
+              const SizedBox(
+                  width: 50,
+                  child: Text(
+                    'Mod',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+            ],
+          ),
+          SizedBox(
+              height: MediaQuery.of(context).size.height / 9,
+              child: ListWheelScrollView(
+                overAndUnderCenterOpacity: .5,
+                itemExtent: 22,
+                children: appState.mods.map<Widget>((e) {
+                  var avg = e * songInfo!.chart.dominantBpm;
+                  var min = e * songInfo!.chart.trueMin;
+                  var max = e * songInfo!.chart.trueMax;
+                  return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 50, child: Text('$avg')),
+                        if (isBpmChange!) ...[
+                          SizedBox(width: 50, child: Text('$min')),
+                          SizedBox(width: 50, child: Text('$max')),
+                        ],
+                        SizedBox(width: 50, child: Text('$e')),
+                      ]
+                          .expand((x) => [const SizedBox(width: 30), x])
+                          .skip(1)
+                          .toList());
+                }).toList(),
+              )),
+        ],
+      ),
+    );
   }
 }
