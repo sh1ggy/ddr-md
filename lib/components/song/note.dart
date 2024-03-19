30989445d692e99678bdf92aeaa9def7fe8b9ba0
sh1ggy <tyronewessnolasco@gmail.com>
2024-03-20 07:35:55 +1000

    Bottom navigation sheet & floating action button

--- lib/components/song/note.dart
+++ lib/components/song/note.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:ddr_md/constants.dart' as Constants;
 
 class NotePage extends StatelessWidget {
+  const NotePage({super.key});
+
   @override
   Widget build(BuildContext context) => SafeArea(
         child: Scaffold(
@@ -14,16 +14,36 @@ class NotePage extends StatelessWidget {
               style: TextStyle(fontSize: 15),
             ),
           ),
-          body: Container(
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.edit),
+            onPressed: () {
+              showModalBottomSheet<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return const NewNoteField();
+                },
+              );
+            },
+          ),
+          body: Padding(
             padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          for (var i = 0; i < 10; i++)
-                            SizedBox(
+            child: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        for (var i = 0; i < 10; i++)
+                          GestureDetector(
+                            onTap: () {
+                              showModalBottomSheet<void>(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return const NewNoteField();
+                                },
+                              );
+                            },
+                            child: SizedBox(
                                 width: MediaQuery.of(context).size.width,
                                 child: const Card(
                                   color: Colors.orangeAccent,
@@ -43,40 +63,68 @@ class NotePage extends StatelessWidget {
                                     ),
                                   ),
                                 )),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber.shade100,
-                      borderRadius: BorderRadius.circular(6),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 10,
-                          color: Colors.grey.shade600,
-                        ),
+                          ),
                       ],
                     ),
-                    padding: const EdgeInsets.all(15),
-                    child: const TextField(
-                      maxLines: 3,
-                      keyboardType: TextInputType.text,
-                      textAlign: TextAlign.center,
-                      textAlignVertical: TextAlignVertical.center,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        focusedErrorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        hintText: 'Enter new note here...',
-                      ),
-                    ),
                   ),
-                ],
-              )),
+                ),
+              ],
+            ),
+          ),
         ),
       );
+}
+
+class NewNoteField extends StatelessWidget {
+  const NewNoteField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.amber.shade100,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.grey.shade600,
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(15),
+      width: MediaQuery.of(context).size.width,
+      child: SizedBox(
+        height: 200,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const TextField(
+                maxLines: 3,
+                keyboardType: TextInputType.text,
+                textAlign: TextAlign.center,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  focusedErrorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  hintText: 'Enter new note here...',
+                ),
+              ),
+              IconButton(
+                  icon: const Icon(Icons.save),
+                  tooltip: "Save note",
+                  onPressed: () => Navigator.pop(context)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
