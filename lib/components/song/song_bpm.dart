40ff7928ddf47957aa739cb359af433b806819d6
sh1ggy <tyronewessnolasco@gmail.com>
2024-04-03 16:59:37 +1000

    Reorganisation

--- lib/components/song/song_bpm.dart
+++ lib/components/song/song_bpm.dart
@@ -3,31 +3,6 @@ import 'package:ddr_md/main.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class SongBpmTextItem extends StatelessWidget {
-  const SongBpmTextItem(
-      {super.key,
-      required this.text,
-      required this.nearestModIndex,
-      required this.e});
-
-  final String text;
-  final int nearestModIndex;
-  final double e;
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<AppState>();
-    return SizedBox(
-      width: 50,
-      child: Text(text,
-          style: TextStyle(
-              color: nearestModIndex == appState.mods.indexOf(e)
-                  ? Colors.white
-                  : Theme.of(context).textTheme.bodyMedium?.color)),
-    );
-  }
-}
-
 class SongBpm extends StatelessWidget {
   const SongBpm(
       {super.key,
@@ -136,6 +111,31 @@ class SongBpm extends StatelessWidget {
               )),
         ],
       ),
+    );
+  }
+}
+
+class SongBpmTextItem extends StatelessWidget {
+  const SongBpmTextItem(
+      {super.key,
+      required this.text,
+      required this.nearestModIndex,
+      required this.e});
+
+  final String text;
+  final int nearestModIndex;
+  final double e;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+    return SizedBox(
+      width: 50,
+      child: Text(text,
+          style: TextStyle(
+              color: nearestModIndex == appState.mods.indexOf(e)
+                  ? Colors.white
+                  : Theme.of(context).textTheme.bodyMedium?.color)),
     );
   }
 }
