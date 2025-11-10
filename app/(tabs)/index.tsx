5578d4ff608fac66dc64e393c0cf69b1261fb020
sh1ggy <tyronewessnolasco@gmail.com>
2025-11-11 07:41:23 +1000

    Working Jotai test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, useColorScheme } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import Card from "@/components/Card";
+import { Text, View } from "@/components/Themed";
+import ThemeSwitch from "@/components/ThemeSwitch";
+import { theme } from "@/stores/global";
+import { useAtom } from "jotai";
 
 export default function TabOneScreen() {
+  const [appTheme] = useAtom(theme);
+  const colorScheme = useColorScheme();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Card title={appTheme} />
+      {colorScheme && <Card title={colorScheme} />}
+      <ThemeSwitch />
     </View>
   );
 }
@@ -16,16 +27,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
