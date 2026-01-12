a764984ab506a980499c7bbe25f2c95dffda1818
sh1ggy <tyronewessnolasco@gmail.com>
2026-01-12 22:30:52 +1000

    chore: flutter create dot

--- android/app/src/main/kotlin/com/example/bpm_clicker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.shiggy.ddrmd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/shiggy/ddr_md/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shiggy.ddr_md
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
