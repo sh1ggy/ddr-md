cc75e03f77c8376e8b094eb9f2f239ab9f0c6d13
sh1ggy <tyronewessnolasco@gmail.com>
2026-01-16 07:44:33 +1000

    wip: attempt

--- native_opencv/ios/Classes/native_opencv.cpp
+++ native_opencv/ios/Classes/native_opencv.cpp
@@ -24,6 +24,9 @@
 using namespace cv;
 using namespace std;
 
+const int max_value_H = 360 / 2;
+const int max_value = 255;
+
 long long int get_now()
 {
     return chrono::duration_cast<std::chrono::milliseconds>(
@@ -63,6 +66,16 @@ extern "C"
                        uint8_t *imageBuffer, int32_t *outputRect)
     {
         long long start = get_now();
+        Mat img(imgHeight, imgWidth, (bytesPerPixel == 4) ? CV_8UC4 : CV_8UC3, imageBuffer);
+
+        Mat frame;
+
+#ifdef __ANDROID__
+        Mat myyuv(imgHeight + imgHeight / 2, imgWidth, CV_8UC4, imageBuffer);
+        cvtColor(myyuv, frame, COLOR_YUV2BGRA_NV21);
+#else
+        frame = Mat(imgHeight, imgWidth, CV_8UC4, imageBuffer);
+#endif
 
         // Mat input = imread(inputImagePath, IMREAD_GRAYSCALE);
         // Mat threshed, withContours;
@@ -71,19 +84,158 @@ extern "C"
         // vector<Vec4i> hierarchy;
 
         // Generate random rectangle within image bounds
-        int rectWidth = 50 + rand() % (imgWidth / 2);
-        int rectHeight = 50 + rand() % (imgHeight / 2);
-        int rectX = rand() % (imgWidth - rectWidth);
-        int rectY = rand() % (imgHeight - rectHeight);
-        
-        Rect randomRect(rectX, rectY, rectWidth, rectHeight);
-       
-        outputRect[0] = randomRect.x;
-        outputRect[1] = randomRect.y;
-        outputRect[2] = randomRect.width;
-        outputRect[3] = randomRect.height;
+        // int rectWidth = 50 + rand() % (imgWidth / 2);
+        // int rectHeight = 50 + rand() % (imgHeight / 2);
+        // int rectX = rand() % (imgWidth - rectWidth);
+        // int rectY = rand() % (imgHeight - rectHeight);
 
-        int evalInMillis = static_cast<int>(get_now() - start);
+        // Rect randomRect(rectX, rectY, rectWidth, rectHeight);
+
+        // outputRect[0] = randomRect.x;
+        // outputRect[1] = randomRect.y;
+        // outputRect[2] = randomRect.width;
+        // outputRect[3] = randomRect.height;
+
+        // int evalInMillis = static_cast<int>(get_now() - start);
+
+        // INIT
+
+        Mat grayImg;
+        cvtColor(img, grayImg, COLOR_BGR2GRAY);
+
+        // Selecting Details box - HSV mask
+        Mat imgHSV;
+        cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+        double channel1Min = 0.380;
+        double channel1Max = 0.531;
+        double channel2Min = 0.204;
+        double channel2Max = 1.000;
+        double channel3Min = 0.592;
+        double channel3Max = 1.000;
+
+        // Use inRange for HSV thresholding
+        Scalar lowerHSV(channel1Min * max_value_H, channel2Min * max_value, channel3Min * max_value);
+        Scalar upperHSV(channel1Max * max_value_H, channel2Max * max_value, channel3Max * max_value);
+
+        Mat BW_HSV;
+        inRange(imgHSV, lowerHSV, upperHSV, BW_HSV);
+
+        // Do blob detection and filter small blobs
+        vector<vector<Point>> contours;
+        findContours(BW_HSV, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        Mat BW2 = Mat::zeros(BW_HSV.size(), CV_8U);
+        for (size_t i = 0; i < contours.size(); i++)
+        {
+            double area = contourArea(contours[i]);
+            if (area >= 3000 && area <= 50000)
+            {
+                drawContours(BW2, contours, i, Scalar(255), FILLED);
+            }
+        }
+
+        int m = 360;
+        int n = 90;
+
+        // Create opening kernel using byte array (faster than getStructuringElement)
+        int open_width = m * 0.1;
+        int open_height = n * 0.1;
+        uchar *open_data = new uchar[open_height * open_width];
+        memset(open_data, 255, open_height * open_width);
+        Mat SE_open(open_height, open_width, CV_8U, open_data);
+
+        auto start_open = chrono::high_resolution_clock::now();
+        Mat BW3;
+        morphologyEx(BW2, BW3, MORPH_OPEN, SE_open);
+        auto end_open = chrono::high_resolution_clock::now();
+        auto duration_open = chrono::duration_cast<chrono::microseconds>(end_open - start_open);
+        cout << "Opening operation with byte array kernel: " << duration_open.count() << " microseconds" << endl;
+
+        // Dont need close for now, works fiune with JUST open to get rid of noise
+
+        //// Create closing kernel using byte array
+        // int close_width = m * 1.2;
+        // int close_height = n * 1.2;
+        // uchar* close_data = new uchar[close_height * close_width];
+        // memset(close_data, 255, close_height * close_width);
+        // Mat SE_close(close_height, close_width, CV_8U, close_data);
+
+        // auto start_close = chrono::high_resolution_clock::now();
+        // Mat BW4;
+        // morphologyEx(BW3, BW4, MORPH_CLOSE, SE_close);
+        // auto end_close = chrono::high_resolution_clock::now();
+        // auto duration_close = chrono::duration_cast<chrono::microseconds>(end_close - start_close);
+        // cout << "Closing operation with byte array kernel: " << duration_close.count() << " microseconds" << endl;
+
+        delete[] open_data;
+        // delete[] close_data;
+
+        // Get bounding boxes
+        vector<vector<Point>> contours_final;
+        findContours(BW3.clone(), contours_final, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        vector<Rect> roi;
+        for (size_t i = 0; i < contours_final.size(); i++)
+        {
+            roi.push_back(boundingRect(contours_final[i]));
+        }
+
+        int numAdditionalPixels = 5;
+        for (size_t i = 0; i < roi.size(); i++)
+        {
+            roi[i].x -= numAdditionalPixels;
+            roi[i].y -= numAdditionalPixels;
+            roi[i].width += 2 * numAdditionalPixels;
+            roi[i].height += 2 * numAdditionalPixels;
+        }
+
+        Mat roi_img = img.clone();
+        for (size_t i = 0; i < roi.size(); i++)
+        {
+            rectangle(roi_img, roi[i], Scalar(0, 255, 0), 4);
+        }
+
+        // OCR Preprocessing
+        Mat Icorrected;
+        Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(31, 31));
+        morphologyEx(img, Icorrected, MORPH_BLACKHAT, kernel);
+
+        Mat Ifiltered;
+        GaussianBlur(Icorrected, Ifiltered, Size(0, 0), 1);
+
+        Mat BW_ocr;
+        cvtColor(Ifiltered, BW_ocr, COLOR_BGR2GRAY);
+
+        Mat BW1;
+        threshold(BW_ocr, BW1, 0, 255, THRESH_BINARY + THRESH_OTSU);
+
+        // Morphological filtering
+        Mat BW2_ocr = Mat::zeros(BW1.size(), CV_8U);
+        vector<vector<Point>> contours_ocr;
+        findContours(BW1.clone(), contours_ocr, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        vector<Rect> ocr_roi;
+        for (size_t i = 0; i < contours_ocr.size(); i++)
+        {
+            if (contourArea(contours_ocr[i]) >= 5)
+            {
+                ocr_roi.push_back(boundingRect(contours_ocr[i]));
+                drawContours(BW2_ocr, contours_ocr, i, Scalar(255), FILLED);
+            }
+        }
+
+        Mat BW3_ocr;
+        bitwise_not(BW2_ocr, BW3_ocr);
+
+        // GOOTTTAAA GOOO 
+        outputRect[0] = ocr_roi[0].x;
+        outputRect[1] = ocr_roi[0].y;
+        outputRect[2] = ocr_roi[0].width;
+        outputRect[3] = ocr_roi[0].height;
+        // outputRect[0] = randomRect.x;
+        // outputRect[1] = randomRect.y;
+        // outputRect[2] = randomRect.width;
+        // outputRect[3] = randomRect.height;
         // platform_log("Processing done in %dms\n", evalInMillis);
         // platform_log("Random rect: x=%d, y=%d, w=%d, h=%d\n", rectX, rectY, rectWidth, rectHeight);
     }
