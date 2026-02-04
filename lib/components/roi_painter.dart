8ada10fddbbd9120c185833ec0ffea88473c6b89
sh1ggy <tyronewessnolasco@gmail.com>
2026-02-04 17:24:09 +1000

    feat: ROI drawing with text labels

--- lib/components/roi_painter.dart
+++ lib/components/roi_painter.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 
@@ -16,28 +17,52 @@ class RoiResultPainter extends CustomPainter {
     ..color = Colors.red.withOpacity(0.1)
     ..style = PaintingStyle.fill;
 
+  final paragraphStyle = ui.ParagraphStyle(
+    textAlign: TextAlign.left,
+    fontSize: 15,
+    fontWeight: ui.FontWeight.bold,
+  );
+
+  final textStyle = ui.TextStyle(
+    color: Colors.amber,
+    shadows: [
+      const Shadow(
+        color: Colors.black,
+        offset: Offset(1, 1),
+        blurRadius: 2,
+      ),
+    ],
+  );
+
   @override
   void paint(Canvas canvas, Size size) {
-    final centerRectWidth = size.width * 0.1;
-    final centerRectHeight = size.height * 0.1;
-
     for (int i = 0; i < rois.length; i++) {
       final roi = rois[i];
+
       final rectToDraw = Rect.fromLTWH(
         roi.left.toDouble(),
         roi.top.toDouble(),
         roi.width.toDouble(),
         roi.height.toDouble(),
       );
+
+      // Draw rect + fill
       canvas.drawRect(rectToDraw, _fillPaint);
       canvas.drawRect(rectToDraw, _paint);
-    }
 
-    // canvas.rotate(pi/2);
+      final builder = ui.ParagraphBuilder(paragraphStyle)
+        ..pushStyle(textStyle)
+        ..addText(i.toString());
+
+      final paragraph = builder.build()
+        ..layout(ui.ParagraphConstraints(width: rectToDraw.width));
+
+      final offset = Offset(rectToDraw.right + 6, rectToDraw.top);
+      canvas.drawParagraph(paragraph, offset);
+    }
   }
 
   @override
-  bool shouldRepaint(RoiResultPainter oldDelegate) {
-    return rois != oldDelegate.rois;
-  }
+  bool shouldRepaint(covariant RoiResultPainter oldDelegate) =>
+      rois != oldDelegate.rois;
 }
